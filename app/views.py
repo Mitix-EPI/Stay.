801478db6224f3db2364e3effba241671e4c751a
Alexandre JUAN <alexandre.juan@epitech.eu>
2020-04-01 00:01:07 +0200

    add python function which created the wolrd add ban ip add register working with good icon for mails

--- app/views.py
+++ app/views.py
@@ -2,7 +2,8 @@ from app import app
 # from flask import render_template
 # from flask import request
 from flask import Flask, render_template, request, redirect, url_for, session
-
+from datetime import datetime, timedelta
+from random import randint
 
 from flask import jsonify
 import pymysql as sql
@@ -37,6 +38,113 @@ def home():
     else:
         return redirect(url_for('signin'))
 
+def check_date(date, value, timer): # Format date: YYYY/MM/JJ HH:MM
+
+    # Cette fonction permet de récupérer la date de première connexion, pour ce faire on vérifie si
+    # la date sans compter l'heure, la minute et plus petit est supérieure à la date précédemment entrée
+    # dans la BDD de l'utilisateur. Si cette valeur est NULL c'est que l'utilisateur ne s'était pas connecté
+    # dans les temps un ancien jour.
+
+    # Dans le cas où la date est différente, on regarde alors l'heure de connexion. Si l'heure est bien entre 8h et
+    # 10h30, alors on set la date actuelle, dans le cas contraire on set NULL
+    # Dans tous les cas sauf lorsqu'il ne s'agit pas de la première connexion de l'utilisateur, le timer des popups est
+    # également set sur NULL mais redéfini dans le fonction juste en dessous de celle-ci.
+
+    new_possible_date = str(date.year) + "/" + str(date.month) + "/" + str(date.day) + " " + str(date.hour) + ":" + str(date.minute)
+
+    if (value == "NULL"): # Si il avait raté un jour
+        actual_hour = [int(date.hour), int(date.minute)]
+
+        if actual_hour[0] >= 8 and actual_hour[0] <= 9:
+            return new_possible_date, "NULL"
+        elif actual_hour[0] == 10:
+            if actual_hour[1] <= 30:
+                return new_possible_date, "NULL"
+            else:
+                return "NULL", "NULL" # Plus tard que 10h30
+        else:
+            return "NULL", "NULL" # Plus tard que 10h30
+    else: # Si il n'avait pas raté le jour
+        tmp = value.split(" ")
+        date_tmp = tmp[0].split("/")
+        hour_tmp = tmp[1].split(":")
+
+        for i in range(0, len(date_tmp)):
+            date_tmp[i] = int(date_tmp[i])
+        for i in range(0, len(hour_tmp)):
+            hour_tmp[i] = int(hour_tmp[i])
+
+        actual_date = [int(date.year), int(date.month), int(date.day)]
+        actual_hour = [int(date.hour), int(date.minute)]
+
+        is_diff = False
+        for i in range(0, len(actual_date)):
+            if actual_date[i] > date_tmp[i]:
+                is_diff = True
+                break
+        if is_diff:
+            if actual_hour[0] >= 8 and actual_hour[0] <= 9:
+                return new_possible_date, "NULL"
+            elif actual_hour[0] == 10:
+                if actual_hour[1] <= 30:
+                    return new_possible_date, "NULL"
+                else:
+                    return "NULL", "NULL" # Plus tard que 10h30
+            else:
+                return "NULL", "NULL" # Plus tard que 10h30
+        else:
+            return value, timer
+    return "NULL", "NULL" # Si error
+
+def set_timer(join_date, timer):
+
+    # Dans le cas où le timer n'est pas NULL et est défini (cas si le joueur ne se connecte pas pour la première fois de la journée
+    # mais qu'il s'est connecté la première fois dans les temps (entre 8h et 10h30)), on vérifie alors que le temps actuel a bien
+    # dépassé l'heure indiquée dans le timer. Si c'est le cas, on fait de même que dans le premier paragraphe: à savoir un random
+    # entre 30 minutes et 2h30
+
+
+    # Pense-bête: Faire en sorte de set à NULL la date et timer si le refresh n'a pas eu lieu dans les 10 min
+    # qui ont suivi la pop-up pour mettre le jour en fail
+    # Penser donc à 18h à ajouter 1 jour à tous les users qui ont donc la date de première connexion != NULL
+
+    if join_date == "NULL":
+        return ("NULL")
+    elif timer == "NULL":
+        time = datetime.now()
+        tmp = timedelta(hours=randint(0, 2), minutes=randint(0, 30))
+        if tmp.hours == 0 and tmp.minutes < 30:
+            tmp.minutes = 30
+        time += tmp
+        if time.hour < 8 or time.hour >= 18:
+            return ("NULL") # 18h passé et les popups s'arrêtent à 18h
+        else:
+            text = str(time.hour) + ":" + str(time.minute)
+            return (text)
+    else:
+        timer_split = timer.split(":")
+        date_join_split = join_date.split(" ")
+        date_hour = date_join_split[1].split(":")
+        del date_join_split
+
+        if int(timer_split[0]) <= int(date_hour[0]):
+            if int(timer_split[1]) <= int(date_hour[1]): # Temps dépassé
+                time = datetime.now()
+                tmp = timedelta(hours=randint(0, 2), minutes=randint(0, 30))
+                if tmp.hours == 0 and tmp.minutes < 30:
+                    tmp.minutes = 30
+                time += tmp
+                if time.hour < 8 or time.hour >= 18:
+                    return ("NULL") # 18h passé et les popups s'arrêtent à 18h
+                else:
+                    text = str(time.hour) + ":" + str(time.minute)
+                    return (text)
+            else:
+                return (timer)
+        else:
+            return (timer)
+    return (timer)
+
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
     msg = ''
@@ -55,11 +163,14 @@ def signin():
         cursor.execute("""SELECT * FROM user WHERE (username = %s OR mail = %s) AND password = %s""", (username, username, password))
         account = cursor.fetchone()
         if (account):
+            tmp, timer = check_date(datetime.now(), account[5], account[6])
             session['loggedin'] = True
             session['id'] = account[0]
             session['username'] = account[1]
             session['mail'] = account[3]
             session['day'] = account[4]
+            session['time'] = tmp
+            session['timer'] = set_timer(tmp, timer)
             return (redirect(url_for('home')))
         else:
             msg = 'Incorrect username/password!'
--- database.sql
+++ database.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS `user` (
     `mail` varchar(255) NOT NULL,
     `ip` varchar(255) NOT NULL,
     `day` int(11) NOT NULL,
-    `time` varchar(255) NOT NULL,
+    `date` varchar(255) NOT NULL,
+    `timer` varchar(255) NOT NULL, -- Ajouté
     PRIMARY KEY (`user_id`),
     UNIQUE KEY `username` (`username`)
 )DEFAULT CHARSET=utf8;
